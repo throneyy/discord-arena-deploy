737e64f5d8d1c3ff8eea3594bf77e1ba847c48de
throneyy <codyjackman@gmail.com>
2026-03-03 20:33:08 -0600

    feat: post startup message to Discord channel on bot ready

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -6,8 +6,18 @@ const logger = require('../../utils/logger');
 module.exports = {
   name: 'ready',
   once: true,
-  execute(client) {
+  async execute(client) {
     logger.info(`Discord bot online as ${client.user.tag}`);
     logger.info(`Serving ${client.guilds.cache.size} guild(s)`);
+
+    // Post startup message to the main channel
+    try {
+      const channel = await client.channels.fetch('1418477778805723163');
+      if (channel) {
+        await channel.send('**Discord Arena Bot is LIVE on Railway** -- all systems online.');
+      }
+    } catch (err) {
+      logger.warn('Could not send startup message', { error: err.message });
+    }
   },
 };
